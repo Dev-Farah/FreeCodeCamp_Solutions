76f9a979dff23d38e8289464edd3cb22e8117889
FarahSyed <farahsyed44@gmail.com>
2022-06-03 16:44:26 +0500

    add challenges 71 to 82

--- Basic JavaScript/app.js
+++ Basic JavaScript/app.js
@@ -792,3 +792,282 @@ function testLogicalOr(val) {
 }
 
 testLogicalOr(15);
+
+
+
+// Challenge 71
+// Introducing Else Statements
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+
+
+// Challenge 72
+// Introducing Else If Statements
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+  else {
+  return "Between 5 and 10";
+  }
+}
+
+testElseIf(7);
+
+
+
+// Challenge 73
+// Logical Order in If Else Statements
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+
+
+// Challenge 74
+// Chaining If Else Statements
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+  // Only change code above this line
+}
+
+testSize(7);
+
+
+
+// Challenge 75
+// Golf Code
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+    return names[0];
+  } else if (strokes <= par - 2)  {
+    return names[1];
+  } else if (strokes == par - 1) {
+    return names[2];
+  } else if (strokes == par) {
+    return names[3];
+  } else if (strokes == par + 1) {
+    return names[4];
+  } else if (strokes == par + 2) {
+    return names[5];
+  } else {
+    return names[6];
+  }
+  // Only change code above this line
+}
+
+golfScore(5, 4);
+
+
+
+// Challenge 76
+// Selecting from Many Options with Switch Statements
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case (1):
+      answer = "alpha";
+      break;
+    case (2):
+      answer = "beta";
+      break;
+    case (3):
+      answer = "gamma";
+      break;
+    case (4):
+      answer = "delta";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
+
+
+// Challenge 77
+// Adding a Default Option in Switch Statements
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+
+
+// Challenge 78
+// Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+    default:
+      answer = "Invalid";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
+
+
+// Challenge 79
+// Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+
+
+// Challenge 80
+// Returning Boolean Values from Functions
+function isLess(a, b) {
+  // Only change code below this line
+    return a < b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
+
+
+
+// Challenge 81
+// Return Early Pattern for Functions
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+
+
+
+// Challenge 82
+// Counting Cards
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  if (card == 2 || card == 3 || card == 4 || card == 5 || card == 6) {
+    count++;
+  } else if (card == 10 || card == 'J' || card == 'Q' || card == 'K' || card == 'A') {
+    count--;
+  }
+var holdBet = "Hold";
+if (count > 0) {
+  holdBet = "Bet";
+}
+return count + " " + holdBet;
+  // Only change code above this line
+}
+// console.log(one);
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+console.log(cc(2), cc(3), cc(4), cc(5), cc(6));
