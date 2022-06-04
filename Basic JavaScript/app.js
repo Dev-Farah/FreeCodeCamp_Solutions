37cfca9038b28f284e858038b2008c1bb9ccfe73
FarahSyed <farahsyed44@gmail.com>
2022-06-04 17:43:13 +0500

    add challenges 83 to 89

--- Basic JavaScript/app.js
+++ Basic JavaScript/app.js
@@ -1071,3 +1071,108 @@ return count + " " + holdBet;
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
 console.log(cc(2), cc(3), cc(4), cc(5), cc(6));
+
+
+
+// Challenge 83
+// Build JavaScript Objects
+const myDog = {
+  // Only change code below this line
+  name: "Tommy",
+  legs: 4,
+  tails: 1,
+  friends: ["Milo", "Fiona"]
+  // Only change code above this line
+};
+
+
+
+// Challenge 84
+// Accessing Object Properties with Dot Notation
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+
+
+// Challenge 85
+// Accessing Object Properties with Bracket Notation
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
+
+
+
+// Challenge 86
+// Accessing Object Properties with Variables
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+
+
+
+// Challenge 87
+// Updating Object Properties
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog.name = "Happy Coder";
+myDog["name"] = "Happy Coder";
+
+
+
+// Challenge 88
+// Add New Properties to a JavaScript Object
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof";
+myDog["bark"] = "woof";
+
+
+
+// Challenge 89
+// Delete Properties from a JavaScript Object
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails;
+delete myDog["tails"];
