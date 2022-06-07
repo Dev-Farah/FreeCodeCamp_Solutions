9131ebdc2ea3a6ca99b98a0b2e9181f7c4477a94
FarahSyed <farahsyed44@gmail.com>
2022-06-07 23:28:32 +0500

    add challenges 101 to 104

--- Basic JavaScript/app.js
+++ Basic JavaScript/app.js
@@ -1395,3 +1395,96 @@ let total = 0;
 for (let i = 0; i < myArr.length; i++) {
   total += myArr[i];
 }
+
+
+
+// Challenge 101
+// Nesting For Loops
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+  product *= arr[i][j];
+  }
+}
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+
+
+// Challenge 102
+// Iterate with JavaScript Do...While Loops
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do { myArray.push(i);
+  i++;
+} while (i < 5);
+
+
+
+// Challenge 103
+// Replace Loops using Recursion
+function sum(arr, n) {
+  // Only change code below this line
+if (n <= 0) {
+  return 0;
+} else {
+  return sum(arr, n - 1) + arr[n - 1];
+}
+  // Only change code above this line
+}
+
+
+
+// Challenge 104
+// Profile Lookup
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (let i = 0; i < contacts.length; i++) {
+    let contact = contacts[i];
+    if (contact.firstName === name && contact.hasOwnProperty(prop)) {
+      return contact[prop];
+    } else if (contact.firstName === name && !contact.hasOwnProperty(prop)) {
+      return "No such property";
+    }
+  }
+  return "No such contact";
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
